cd9470d59fbbc3013585af7a4608106197a6628d
Dan <guaneec@gmail.com>
2020-09-20 01:29:13 +0800

    Change function signatures

--- quiz1.c
+++ quiz1.c
@@ -42,9 +42,9 @@ void remove_entry(node_t **head, node_t *entry)
     free(entry);
 }
 
-node_t *swap_pair(node_t *head)
+void swap_pair(node_t **head)
 {
-    for (node_t **node = &head; *node && (*node)->next; /* BB1 */
+    for (node_t **node = head; *node && (*node)->next; /* BB1 */
          node = &(*node)->next->next) {
         node_t *tmp = *node;
         /* BB2  */
@@ -52,21 +52,20 @@ node_t *swap_pair(node_t *head)
         tmp->next = (*node)->next;
         (*node)->next = tmp;
     }
-    return head;
 }
 
-node_t *reverse(node_t *head)
+void reverse(node_t **head)
 {
     node_t *cursor = NULL;
-    while (head) {
-        node_t *next = head->next;
+    while (*head) {
+        node_t *next = (*head)->next;
         // CCC;
-        head->next = cursor;
-        cursor = head;
+        (*head)->next = cursor;
+        cursor = (*head);
 
-        head = next;
+        (*head) = next;
     }
-    return cursor;
+    *head = cursor;
 }
 
 void print_list(node_t *head)
@@ -102,10 +101,10 @@ int main(int argc, char const *argv[])
     /* swap pair.
      * See https://leetcode.com/problems/swap-nodes-in-pairs/
      */
-    head = swap_pair(head);
+    swap_pair(&head);
     print_list(head);
 
-    head = reverse(head);
+    reverse(&head);
     print_list(head);
 
     return 0;
